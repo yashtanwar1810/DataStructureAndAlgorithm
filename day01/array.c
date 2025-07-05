6a19c25a4755ecfbeb47b52a1d3035a4678f03d1
Yash Tanwar <yashtanwar810@gmail.com>
2025-07-05 19:16:11 +0530

    first array program

--- day01/array.c
+++ day01/array.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello GitHub CodeSpaces :)\n");
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *ptr = arr; // Pointer to the first element of the array
+    int i;
+    // Print the elements of the array using pointer arithmetic
+    for(i = 0; i < 5; i++){
+        printf("%d ", *(ptr + i)); // Accessing elements using pointer arithmetic
+    }
+    
     return 0;
 }
